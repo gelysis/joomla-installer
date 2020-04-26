d5fb6a1a67e11fbd94e080eed0064f1d9922f387
gelysis <age@gelysis.org>
2020-04-26 18:50:12 +1200

    Fixed Joomla CMS .git folder path and added installation soft link

--- src/install-cms.sh
+++ src/install-cms.sh
@@ -10,11 +10,12 @@ git config --global advice.detachedHead false
 git clone --quiet $REPOSITORY --branch $VERSION --depth 1 --single-branch $FOLDER > /dev/null
 git config --global advice.detachedHead true
 
-rm -rf $FOLDER/git composer.*
+rm -rf $FOLDER/.git composer.*
 mv $FOLDER/composer.* ./
 
 cd ./public
 ln -s ../joomla-cms/index.php index.php
+ln -s ../joomla-cms/installation installation
 ln -s ../joomla-cms/media media
 ln -s ../joomla-cms/templates templates
 
